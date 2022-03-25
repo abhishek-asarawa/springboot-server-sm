42adc9b8032e0b4c12b09855aaeec960e030a572
Abhishek Asarawa <100663337+abhishek-eubrics@users.noreply.github.com>
2022-03-25 11:55:58 +0530

    Setting simle springboot server

---
+++ src/main/java/com/service/servicescriptmed/ServiceScriptmedApplication.java
@@ -0,0 +1,13 @@
+package com.service.servicescriptmed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceScriptmedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceScriptmedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/service/servicescriptmed/ServiceScriptmedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.servicescriptmed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceScriptmedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
